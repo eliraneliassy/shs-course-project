bc5e26dd9dac4ad4221bd7e9c166537be5b1790d
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-07-02 11:46:34 +0300

    app-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { db } from './db';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shs-course-project';
+export class AppComponent implements OnInit {
+  items: any[] = db;
+  ngOnInit(): void {
+    console.log(db);
+  }
 }
