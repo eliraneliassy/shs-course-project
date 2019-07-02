46183872ea2f61893cbac9637cb5cbab9844ada5
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-07-02 16:42:53 +0300

    add httpclient client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { db } from './db';
 import { Item } from './item';
+import { FeedService } from './services/feed.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-  items: Item[] = db;
+
+  
+  //items: Item[] = [];
+  items$: Observable<Item[]>;
   shoppingCart: Item[] = [];
 
+  constructor(private feedService: FeedService) {}
+
   ngOnInit(): void {
-    console.log(db);
+    //this.feedService.getFeed(0).subscribe((items: Item[]) => this.items = items);
+    this.items$ = this.feedService.getFeed(0);
   }
 
   addToCart(item: Item) {
