aea78516630d1c8af43996f6537625bd1cce302f
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-07-23 11:32:51 +0300

    add effects

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { AuthActionTypes, UserLoginSuccess } from './auth-actions.actions';
+import { switchMap, map, tap } from 'rxjs/operators';
+import { Action } from '@ngrx/store';
+import { Router } from '@angular/router';
+
 
 
 
 @Injectable()
 export class AuthEffects {
 
+  constructor(private actions$: Actions,
+    private router: Router) { }
 
+  @Effect()
+  userLogin$ = this.actions$.pipe(
+    ofType(AuthActionTypes.UserLogin),
+    map((action: any) => new UserLoginSuccess(action.payload))
+  )
 
-  constructor(private actions$: Actions) {}
-
+  @Effect({ dispatch: false })
+  userLoginSuccess$ = this.actions$.pipe(
+    ofType(AuthActionTypes.UserLoginSuccess),
+    tap(() => this.router.navigateByUrl('/feed'))
+  )
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Store, State } from '@ngrx/store';
+import { UserLogin } from '../auth-actions.actions';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +11,15 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
 
   name: string = 'eliran';
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private store: Store<any>) { }
 
   ngOnInit() {
   }
 
   submit(form) {
-    this.authService.login(form.value['email']);
+    // this.authService.login(form.value['email']);
+    this.store.dispatch(new UserLogin(form.value['email']));
 
   }
 
